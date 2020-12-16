8600a2ca7c9b59ff2fad1a47fa9544f3671259ec
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2020-12-16 09:51:21 +0000

    basic temperature convertion

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
-//kelvin value will be fixed to 293
-const kelvin = 293;
+// //kelvin value will be fixed to 293
+// const kelvin = 293;
 
+const kelvin = prompt('Enter the temperature in kelvin degress: ');
+
+//converting Kelvin to Celcius
+const celcius = kelvin - 273;
+
+//fahrenheit value will be changed
+let fahrenheit = celcius * (9 / 5) + 32;
+
+//rounding decimal results
+fahrenheit = Math.round(fahrenheit);
+
+// console.log(`The temperature is ${fahrenheit} degrees fahrenheit`);
+alert(
+	`The temperature is ${celcius} degrees celcius and ${fahrenheit} degrees fahrenheit`,
+);
